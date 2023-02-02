ed3ab530c44e43b89e8aeff8aaebec12c75de73f
Rahul Verma <extreme@Rahuls-MacBook-Air.local>
2023-02-02 23:30:44 +0530

    added dependencies

--- routes/todo.js
+++ routes/todo.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const addController = require('../controller/add');
+const removeController = require('../controller/remove');
+const editController = require('../controller/edit');
 
 router.use(bodyParser.urlencoded({ extended: false }));  
 router.post('/add', addController.add);
+router.post('/remove', removeController.remove);
+router.post('/edit', editController.edit);
 
 module.exports = router;
